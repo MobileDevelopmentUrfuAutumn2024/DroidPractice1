faf03edfeb33d87fa0bad4effafc3474a12cbd33
Denis <dmatafonov2000@mail.ru>
2024-10-01 11:28:57 +0500

    example

--- app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
@@ -1,12 +1,18 @@
 package ru.urfu.droidpractice1
 
+import android.content.Intent
 import androidx.activity.ComponentActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.addCallback
+import com.bumptech.glide.Glide
 import ru.urfu.droidpractice1.databinding.ActivitySecondBinding
 
 class SecondActivity : ComponentActivity() {
 
     private lateinit var binding: ActivitySecondBinding
+
+    private var checked: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +21,21 @@ class SecondActivity : ComponentActivity() {
         setContentView(view)
 
         binding.toolbar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+
+        Glide.with(binding.photo)
+            .asBitmap()
+            .load("https://img.championat.com/s/1350x900/news/big/u/i/igrok-krylev-kostanca-obyasn.jpg")
+            .into(binding.photo)
+
+        binding.switchRead.setOnCheckedChangeListener { _, isChecked -> checked = isChecked }
+
+        onBackPressedDispatcher.addCallback(this) {
+            setResult(RESULT_OK, Intent().apply { putExtra(KEY_READ, checked) })
+            finish()
+        }
+    }
+
+    companion object {
+        const val KEY_READ = "READ"
     }
 }
--- app/src/main/java/ru/urfu/droidpractice1/content/MainActivityScreen.kt
+++ app/src/main/java/ru/urfu/droidpractice1/content/MainActivityScreen.kt
@@ -2,25 +2,39 @@
 
 package ru.urfu.droidpractice1.content
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LargeTopAppBar
-import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import ru.urfu.droidpractice1.MainScreenHandler
 import ru.urfu.droidpractice1.R
 import ru.urfu.droidpractice1.ui.theme.DroidPractice1Theme
+import ru.urfu.droidpractice1.ui.theme.Typography
 
 @Composable
-fun MainActivityScreen() {
+fun MainActivityScreen(
+    handler: MainScreenHandler,
+    read: Boolean = false
+) {
     DroidPractice1Theme {
         Scaffold(modifier = Modifier.fillMaxSize(),
             topBar = {
@@ -29,13 +43,59 @@ fun MainActivityScreen() {
                         Text(
                             text = stringResource(id = R.string.article_title)
                         )
+                    },
+                    actions = {
+                        Icon(
+                            modifier = Modifier.clickable { handler.onToShareClicked() },
+                            painter = painterResource(id = R.drawable.share),
+                            contentDescription = null
+                        )
                     }
                 )
             }) { innerPadding ->
-            Box(
-                modifier = Modifier.padding(innerPadding)
+            Column(
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+                    .padding(innerPadding)
+                    .padding(16.dp)
             ) {
+                Text(text = "Лукас Вера сравнил «Химки» с «Челси»", style = Typography.titleLarge)
 
+                Text(
+                    text = "Полузащитник «Химок» Лукас Вера сравнил подмосковную команду с лондонским «Челси».\n" +
+                            "\n" +
+                            "— Общественность сравнивает «Химки» с «Челси». Количество игроков — единственная схожесть?\n" +
+                            "— Есть такое. Сейчас очень много игроков в команде, но самое важное, что мы хорошо работаем. Никаких проблем я не вижу.\n",
+                    modifier = Modifier.padding(top = 16.dp)
+                )
+
+                AsyncImage(
+                    model = "https://s-cdn.sportbox.ru/images/styles/upload/fp_fotos/1e/6a/74e9bfc4ca26735ebd52f1e8cf8495da66a8b457a4918095248260.jpg",
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(8.dp))
+                )
+
+                Text(
+                    modifier = Modifier.padding(top = 16.dp),
+                    text = "— В чём «Химки» лучше «Челси»?\n" +
+                            "— Это другая лига. У нас другой стиль. Мы делаем на поле то, что просит наш тренер Франк Артига. Но пока очков не так много у нас, — сказал Вера в беседе с корреспондентом «Чемпионата» Ильёй Никульниковым.\n" +
+                            "\n" +
+                            "После 10 туров РПЛ сезона-2024/2025 «Химки» набрали восемь очков и занимают 14-е место в турнирной таблице."
+                )
+
+                Card(
+                    modifier = Modifier
+                        .padding(top = 16.dp)
+                        .clickable { handler.onToOtherScreenClicked() }
+                ) {
+                    Text(
+                        text = "Игрок «Крыльев» Костанца объяснил, чего не хватило для победы в матче с «Химками»",
+                        modifier = Modifier.padding(16.dp),
+                        color = if (read) Color.Gray else Color.Black
+                    )
+                }
             }
         }
     }
@@ -46,5 +106,14 @@ fun MainActivityScreen() {
 @Preview(showBackground = true)
 @Composable
 fun MainScreenPreview() {
-    MainActivityScreen()
+    MainActivityScreen(handler = object : MainScreenHandler {
+        override fun onToOtherScreenClicked() {
+
+        }
+
+        override fun onToShareClicked() {
+
+        }
+
+    })
 }

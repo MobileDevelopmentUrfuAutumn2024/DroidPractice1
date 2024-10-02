ac2cf7561ddbb49584d4f471dcc3dde02e21d8cb
Lapoushko <45125347+Lapoushko@users.noreply.github.com>
2024-10-02 11:42:06 +0500

    new imports

--- app/src/main/java/ru/urfu/droidpractice1/content/MainActivityScreen.kt
+++ app/src/main/java/ru/urfu/droidpractice1/content/MainActivityScreen.kt
@@ -2,25 +2,31 @@
 
 package ru.urfu.droidpractice1.content
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LargeTopAppBar
-import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import ru.urfu.droidpractice1.R
+import ru.urfu.droidpractice1.data.Article
+import ru.urfu.droidpractice1.data.getFragments
 import ru.urfu.droidpractice1.ui.theme.DroidPractice1Theme
 
 @Composable
 fun MainActivityScreen() {
+    val article = Article.articles[0]
+    val texts : List<String> = article.getFragments()
+
     DroidPractice1Theme {
         Scaffold(modifier = Modifier.fillMaxSize(),
             topBar = {
@@ -32,15 +38,27 @@ fun MainActivityScreen() {
                     }
                 )
             }) { innerPadding ->
-            Box(
+            Column(
                 modifier = Modifier.padding(innerPadding)
             ) {
-
+                Text(
+                    text = texts[0],
+                )
+                AsyncImage(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(50.dp),
+                    model = Article.articles[0].path,
+                    contentDescription = null,
+                    alignment = Alignment.Center,
+                )
+                Text(
+                    text = texts[1],
+                )
             }
         }
     }
 }
-
 
 
 @Preview(showBackground = true)

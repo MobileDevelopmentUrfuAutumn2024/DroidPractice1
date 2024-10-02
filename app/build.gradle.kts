0fcf953d69fc1bc6f9778a464bb77f7d67e488ef
Lapoushko <45125347+Lapoushko@users.noreply.github.com>
2024-10-02 11:41:45 +0500

    new libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
+    implementation(libs.coil.compose)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))

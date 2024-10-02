4e5b38dad442dcc64bba5453f77ea85e03c5a4ad
Lapoushko <45125347+Lapoushko@users.noreply.github.com>
2024-10-02 15:41:20 +0500

    edit main activity

--- app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
@@ -1,15 +1,46 @@
 package ru.urfu.droidpractice1
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import ru.urfu.droidpractice1.SecondActivity.Companion.READ_KEY
 import ru.urfu.droidpractice1.content.MainActivityScreen
+import ru.urfu.droidpractice1.data.Article
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), MainScreenHandler {
+
+    private var readed by mutableStateOf(false)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainActivityScreen()
+            MainActivityScreen(this, read = readed)
+        }
+    }
+
+    override fun onToArticleClick() {
+        val intent = Intent(this, SecondActivity::class.java)
+        resultLauncher.launch(intent)
+    }
+
+    override fun onToShareClick() {
+        val share = Intent(Intent.ACTION_SEND)
+        share.type = "text/plain"
+        share.putExtra(Intent.EXTRA_TEXT, Article.articles[0].name)
+        startActivity(Intent.createChooser(share, R.string.send_article.toString()))
+    }
+
+    private val resultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){ result ->
+        if (result.resultCode == RESULT_OK) {
+            val data: Intent? = result.data
+            readed = data?.getBooleanExtra(READ_KEY, false) ?: false
         }
     }
 }
--- app/src/main/java/ru/urfu/droidpractice1/content/MainActivityScreen.kt
+++ app/src/main/java/ru/urfu/droidpractice1/content/MainActivityScreen.kt
@@ -2,31 +2,49 @@
 
 package ru.urfu.droidpractice1.content
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import ru.urfu.droidpractice1.MainScreenHandler
 import ru.urfu.droidpractice1.R
 import ru.urfu.droidpractice1.data.Article
 import ru.urfu.droidpractice1.data.getFragments
 import ru.urfu.droidpractice1.ui.theme.DroidPractice1Theme
+import ru.urfu.droidpractice1.ui.theme.Typography
 
+/**
+ * Главный экран
+ */
 @Composable
-fun MainActivityScreen() {
+fun MainActivityScreen(
+    handler: MainScreenHandler,
+    read: Boolean = false
+) {
     val article = Article.articles[0]
-    val texts : List<String> = article.getFragments()
-
+    val texts: List<String> = article.getFragments()
     DroidPractice1Theme {
         Scaffold(modifier = Modifier.fillMaxSize(),
             topBar = {
@@ -35,26 +53,74 @@ fun MainActivityScreen() {
                         Text(
                             text = stringResource(id = R.string.article_title)
                         )
+                    },
+                    actions = {
+                        Icon(
+                            modifier = Modifier
+                                .clickable { handler.onToShareClick() }
+                                .padding(10.dp),
+                            painter = painterResource(id = R.drawable.share),
+                            contentDescription = null
+                        )
                     }
                 )
             }) { innerPadding ->
             Column(
-                modifier = Modifier.padding(innerPadding)
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+                    .padding(innerPadding)
+                    .padding(10.dp)
             ) {
                 Text(
+                    modifier = Modifier
+                        .padding(20.dp),
+                    text = article.name,
+                    fontSize = 20.sp,
+                    style = Typography.titleLarge,
+                    fontStyle = FontStyle.Italic
+                )
+
+                Text(
+                    modifier = Modifier
+                        .padding(start = 8.dp),
                     text = texts[0],
+                    fontSize = 16.sp
                 )
                 AsyncImage(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(50.dp),
-                    model = Article.articles[0].path,
-                    contentDescription = null,
-                    alignment = Alignment.Center,
+                        .clip(RoundedCornerShape(15.dp)),
+                    model = Article.articles[0].path[0],
+                    contentDescription = null
                 )
                 Text(
+                    modifier = Modifier
+                        .padding(8.dp),
                     text = texts[1],
+                    fontSize = 16.sp
                 )
+
+                Card(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .clickable {
+                            handler.onToArticleClick()
+                        },
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color.Yellow,
+                        contentColor = Color.DarkGray
+                    )
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .padding(20.dp),
+                        style = Typography.titleLarge,
+                        fontStyle = FontStyle.Italic,
+                        text = Article.articles[1].name,
+                        fontSize = 20.sp,
+                        color = if (read) Color.LightGray else Color.Black
+                    )
+                }
             }
         }
     }
@@ -64,5 +130,13 @@ fun MainActivityScreen() {
 @Preview(showBackground = true)
 @Composable
 fun MainScreenPreview() {
-    MainActivityScreen()
+    MainActivityScreen(handler = object : MainScreenHandler {
+        override fun onToArticleClick() {
+
+        }
+
+        override fun onToShareClick() {
+        }
+
+    })
 }

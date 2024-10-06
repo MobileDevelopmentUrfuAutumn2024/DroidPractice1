96ab4d2362d4de4cd46eee00dee7883dd507d1ee
Roman <romamail.ru365@gmail.com>
2024-10-06 21:47:13 +0500

    add compose and xml articles

--- app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
@@ -3,13 +3,26 @@ package ru.urfu.droidpractice1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import ru.urfu.droidpractice1.content.MainActivityScreen
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), MainScreenHandler {
+    private var read: Boolean by mutableStateOf(false)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainActivityScreen()
+            MainActivityScreen(this, read)
         }
+    }
+
+    override fun onToOtherScreenClicked() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onToShareClicked() {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/ru/urfu/droidpractice1/content/MainActivityScreen.kt
+++ app/src/main/java/ru/urfu/droidpractice1/content/MainActivityScreen.kt
@@ -2,49 +2,102 @@
 
 package ru.urfu.droidpractice1.content
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LargeTopAppBar
-import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import ru.urfu.droidpractice1.MainScreenHandler
 import ru.urfu.droidpractice1.R
 import ru.urfu.droidpractice1.ui.theme.DroidPractice1Theme
+import ru.urfu.droidpractice1.ui.theme.Typography
 
 @Composable
-fun MainActivityScreen() {
+fun MainActivityScreen(
+    handler: MainScreenHandler,
+    read: Boolean = false
+) {
     DroidPractice1Theme {
         Scaffold(modifier = Modifier.fillMaxSize(),
             topBar = {
                 TopAppBar(
                     title = {
                         Text(
-                            text = stringResource(id = R.string.article_title)
+                            text = stringResource(id = R.string.article_title),
+                            style = Typography.titleSmall
                         )
                     }
                 )
-            }) { innerPadding ->
-            Box(
-                modifier = Modifier.padding(innerPadding)
+            },
+        ) { innerPadding ->
+        Column (
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+                    .padding(innerPadding)
+                    .padding(16.dp)
             ) {
-
-            }
+            Text(
+                text = stringResource(id = R.string.main_article_header),
+                style = Typography.titleLarge
+            )
+            AsyncImage(
+                model = "https://img.championat.com/s/732x488/news/big/k/m/komandy-nhl-kotorye-mogut-sovershit-proryv_1727790877856269313.jpg",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp))
+                    .padding(top = 10.dp),
+                contentDescription = null
+            )
+            Text(
+                text = stringResource(id = R.string.main_article_title),
+                style = Typography.titleMedium,
+                modifier = Modifier.padding(top = 10.dp)
+            )
+            Text(
+                text = stringResource(id = R.string.main_article_text),
+                style = Typography.bodyLarge,
+                modifier = Modifier.padding(top = 15.dp)
+            )
+            Text(
+                text = stringResource(id = R.string.main_article_text_second),
+                style = Typography.bodyLarge,
+                modifier = Modifier.padding(top = 15.dp)
+            )
+        }
         }
     }
 }
 
-
-
 @Preview(showBackground = true)
 @Composable
 fun MainScreenPreview() {
-    MainActivityScreen()
+    MainActivityScreen(handler = object : MainScreenHandler {
+        override fun onToOtherScreenClicked() {
+
+        }
+
+        override fun onToShareClicked() {
+
+        }
+
+    })
 }

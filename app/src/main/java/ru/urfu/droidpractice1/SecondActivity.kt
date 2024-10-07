4f052641dcb47655a098eeeba38380b0fb92dede
Nikita Krilov <114663524+Nikkri20@users.noreply.github.com>
2024-10-07 23:46:13 +0500

    Работа-2

--- app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
@@ -1,19 +1,40 @@
 package ru.urfu.droidpractice1
 
-import androidx.activity.ComponentActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import ru.urfu.droidpractice1.databinding.ActivitySecondBinding
+import android.widget.Button
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatActivity
 
-class SecondActivity : ComponentActivity() {
+class SecondActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySecondBinding
+    private var isRead: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySecondBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(R.layout.activity_second)
 
-        binding.toolbar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+        val sharedPreferences = getSharedPreferences("article_prefs", Context.MODE_PRIVATE)
+
+        val switchRead = findViewById<Switch>(R.id.switchRead)
+        val buttonBack = findViewById<Button>(R.id.buttonBack)
+
+        isRead = sharedPreferences.getBoolean("second_article_read", false)
+        switchRead.isChecked = isRead
+
+        switchRead.setOnCheckedChangeListener { _, isChecked ->
+            isRead = isChecked
+            sharedPreferences.edit().putBoolean("second_article_read", isRead).apply()
+        }
+
+
+        buttonBack.setOnClickListener {
+            val resultIntent = Intent().apply {
+                putExtra("article_read_status", isRead)
+            }
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
     }
 }

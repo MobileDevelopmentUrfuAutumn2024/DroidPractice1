d0bdff07e49bd200e286cf8ed701e39c8e890f05
lightskintyga <daniilivanov7010@gmail.com>
2024-10-08 02:01:49 +0500

    Практика выполнена!

--- app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
@@ -1,15 +1,96 @@
 package ru.urfu.droidpractice1
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import ru.urfu.droidpractice1.SecondActivity.Companion.KEY_READ
 import ru.urfu.droidpractice1.content.MainActivityScreen
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), MainScreenHandler {
+    private var read: Boolean by mutableStateOf(false)
+
+    override var likesCounter: Int by mutableIntStateOf(0)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainActivityScreen()
+            MainActivityScreen(this, read)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity", "onPause")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MainActivity", "onRestart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity", "onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("likes", likesCounter)
+        outState.putBoolean("read", read)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        likesCounter = savedInstanceState.getInt("likes")
+        read = savedInstanceState.getBoolean("read")
+    }
+
+    override fun onToShareClicked() {
+        val share = Intent(Intent.ACTION_SEND)
+        share.setType("text/plain")
+        share.putExtra(Intent.EXTRA_TEXT, "Как Рио Фердинанд пошёл за бутербродом и пропустил допинг-тест")
+        startActivity(Intent.createChooser(share, "Переслать статью"))
+    }
+
+    override fun onToOtherScreenClicked() {
+        val intent = Intent(this, SecondActivity::class.java)
+        resultLauncher.launch(intent)
+    }
+
+    override fun onLike() {
+        ++likesCounter
+    }
+
+    override fun onDislike() {
+        --likesCounter
+    }
+
+    private val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val data: Intent? = result.data
+            read = data?.getBooleanExtra(KEY_READ, false) ?: false
         }
     }
 }

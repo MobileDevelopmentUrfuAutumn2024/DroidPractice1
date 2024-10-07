9c8d324b9d51f77577c3182f7647d1c1328c5da3
TIMERBOT1 <timbot_hovki@mail.ru>
2024-10-07 15:09:13 +0500

    First Push

---
+++ app/src/main/java/ru/urfu/droidpractice1/HandlerMainScreen.kt
@@ -0,0 +1,5 @@
+package ru.urfu.droidpractice1
+
+interface HandlerMainScreen {
+    fun toSecondActivityClick()
+}
--- app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
@@ -1,15 +1,36 @@
 package ru.urfu.droidpractice1
 
 import android.os.Bundle
+import android.content.Intent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.getValue
 import ru.urfu.droidpractice1.content.MainActivityScreen
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), HandlerMainScreen {
+
+    private var read: Boolean by mutableStateOf(false)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainActivityScreen()
+            MainActivityScreen(this, read)
         }
     }
+
+    override fun toSecondActivityClick() {
+        intent = Intent(this, SecondActivity::class.java).putExtra("state_switch", read)
+        resultLauncher.launch(intent)
+    }
+
+    private val resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data: Intent? = result.data
+                read = data?.getBooleanExtra("READ", false  ) ?: true
+            }
+        }
 }

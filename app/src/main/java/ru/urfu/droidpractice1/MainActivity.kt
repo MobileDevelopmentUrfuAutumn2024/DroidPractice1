6f69b5957af67f8eeb024777f707e6a9b803d397
bns <bns@ap-soft.ru>
2024-10-07 21:08:46 +0500

    Бураков Никита

--- app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
@@ -1,15 +1,114 @@
 package ru.urfu.droidpractice1
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import ru.urfu.droidpractice1.content.MainActivityScreen
 
 class MainActivity : ComponentActivity() {
+    private var countLike: Int by mutableIntStateOf(0)
+    private var countDislike: Int by mutableIntStateOf(0)
+    private var isRead: Boolean by mutableStateOf(false)
+    private lateinit var secondActivityLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainActivityScreen()
+            MainActivityScreen(
+                ::like,
+                ::dislike,
+                countLike,
+                countDislike,
+                isRead,
+                ::clickActivity,
+                ::shareArticle
+            )
         }
+    }
+
+    override fun onStart() {
+        Log.d("MainActivity", "onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d("MainActivity", "onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d("MainActivity", "onPause")
+        super.onPause()
+    }
+
+    override fun onRestart() {
+        Log.d("MainActivity", "onRestart")
+        super.onRestart()
+    }
+
+    override fun onStop() {
+        Log.d("MainActivity", "onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d("MainActivity", "onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("likesCount", countLike)
+        outState.putInt("dislikesCount", countDislike)
+        outState.putBoolean("read", isRead)
+        Log.d("MainActivity", "onSaveInstanceState")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        countLike = savedInstanceState.getInt("likesCount")
+        countDislike = savedInstanceState.getInt("dislikesCount")
+        isRead = savedInstanceState.getBoolean("read")
+        Log.d("MainActivity", "onRestoreInstanceState")
+    }
+
+    private fun like() {
+        countLike++
+    }
+
+    private fun dislike() {
+        countDislike++
+    }
+
+    private val resultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+            result ->
+        if (result.resultCode == RESULT_OK) {
+            val data: Intent? = result.data
+            isRead = data?.getBooleanExtra("read", false) ?: false
+        }
+    }
+
+    private fun clickActivity() {
+        val intent = Intent(this, SecondActivity::class.java)
+            .apply {
+                putExtra("read", isRead)
+            }
+        resultLauncher.launch(intent)
+    }
+
+    private fun shareArticle() {
+        val share = Intent(Intent.ACTION_SEND)
+        share.setType("text/plain")
+        startActivity((Intent.createChooser(share, "Поделиться")))
     }
 }
--- app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
@@ -1,12 +1,16 @@
 package ru.urfu.droidpractice1
 
-import androidx.activity.ComponentActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
 import ru.urfu.droidpractice1.databinding.ActivitySecondBinding
 
 class SecondActivity : ComponentActivity() {
 
     private lateinit var binding: ActivitySecondBinding
+    private var isChecked: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +18,62 @@ class SecondActivity : ComponentActivity() {
         val view = binding.root
         setContentView(view)
 
-        binding.toolbar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+        binding.apply {
+
+            toolbar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+
+            isChecked = intent.getBooleanExtra("read", false)
+            isReadSwitch.isChecked = isChecked
+            isReadSwitch.setOnCheckedChangeListener { _, checked -> isChecked = checked }
+        }
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                setResult(RESULT_OK, Intent().apply { putExtra("read", isChecked) })
+                finish()
+            }
+        })
+    }
+
+    override fun onStart() {
+        Log.d("SecondActivity", "onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d("SecondActivity", "onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d("SecondActivity", "onPause")
+        super.onPause()
+    }
+
+    override fun onRestart() {
+        Log.d("SecondActivity", "onRestart")
+        super.onRestart()
+    }
+
+    override fun onStop() {
+        Log.d("SecondActivity", "onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d("SecondActivity", "onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("read", isChecked)
+        Log.d("SecondActivity", "onSaveInstanceState")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        isChecked = savedInstanceState.getBoolean("read")
+        Log.d("SecondActivity", "onRestoreInstanceState")
     }
 }

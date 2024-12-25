fb75d0fb83d3833f4a11dbf7004806d439ae9c03
Saw4uk <saw4uk.vanya2017@yandex.ru>
2024-12-25 15:10:48 +0500

    Final

--- app/src/main/java/ru/urfu/droidpractice1/content/MainActivityScreen.kt
+++ app/src/main/java/ru/urfu/droidpractice1/content/MainActivityScreen.kt
@@ -2,17 +2,20 @@
 
 package ru.urfu.droidpractice1.content
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -20,20 +23,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import ru.urfu.droidpractice1.MainScreenHandler
 import ru.urfu.droidpractice1.R
 import ru.urfu.droidpractice1.ui.theme.DroidPractice1Theme
 import ru.urfu.droidpractice1.ui.theme.Typography
 
 @Composable
 fun MainActivityScreen(
-    handler: MainScreenHandler,
-    read: Boolean = false
+    onOtherArticleCardClicked: () -> Unit = {},
+    onShareClicked: () -> Unit = {},
+    onLikeClicked: () -> Unit = {},
+    onDislikeClicked: () -> Unit = {},
+    isSecondArticleRead: Boolean = false,
+    likesCount: Int = 0,
+    dislikesCount: Int = 0
 ) {
     DroidPractice1Theme {
         Scaffold(modifier = Modifier.fillMaxSize(),
@@ -46,9 +57,9 @@ fun MainActivityScreen(
                     },
                     actions = {
                         Icon(
-                            modifier = Modifier.clickable { handler.onToShareClicked() },
+                            modifier = Modifier.clickable { onShareClicked.invoke() },
                             painter = painterResource(id = R.drawable.share),
-                            contentDescription = null
+                            contentDescription = "Кнопка поделиться"
                         )
                     }
                 )
@@ -57,63 +68,122 @@ fun MainActivityScreen(
                 modifier = Modifier
                     .verticalScroll(rememberScrollState())
                     .padding(innerPadding)
-                    .padding(16.dp)
+                    .padding(horizontal = 16.dp)
             ) {
-                Text(text = "Лукас Вера сравнил «Химки» с «Челси»", style = Typography.titleLarge)
-
-                Text(
-                    text = "Полузащитник «Химок» Лукас Вера сравнил подмосковную команду с лондонским «Челси».\n" +
-                            "\n" +
-                            "— Общественность сравнивает «Химки» с «Челси». Количество игроков — единственная схожесть?\n" +
-                            "— Есть такое. Сейчас очень много игроков в команде, но самое важное, что мы хорошо работаем. Никаких проблем я не вижу.\n",
-                    modifier = Modifier.padding(top = 16.dp)
-                )
-
-                AsyncImage(
-                    model = "https://s-cdn.sportbox.ru/images/styles/upload/fp_fotos/1e/6a/74e9bfc4ca26735ebd52f1e8cf8495da66a8b457a4918095248260.jpg",
-                    contentDescription = null,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clip(RoundedCornerShape(8.dp))
-                )
-
-                Text(
-                    modifier = Modifier.padding(top = 16.dp),
-                    text = "— В чём «Химки» лучше «Челси»?\n" +
-                            "— Это другая лига. У нас другой стиль. Мы делаем на поле то, что просит наш тренер Франк Артига. Но пока очков не так много у нас, — сказал Вера в беседе с корреспондентом «Чемпионата» Ильёй Никульниковым.\n" +
-                            "\n" +
-                            "После 10 туров РПЛ сезона-2024/2025 «Химки» набрали восемь очков и занимают 14-е место в турнирной таблице."
-                )
-
-                Card(
-                    modifier = Modifier
-                        .padding(top = 16.dp)
-                        .clickable { handler.onToOtherScreenClicked() }
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    modifier = Modifier.padding(bottom = 32.dp)
                 ) {
-                    Text(
-                        text = "Игрок «Крыльев» Костанца объяснил, чего не хватило для победы в матче с «Химками»",
-                        modifier = Modifier.padding(16.dp),
-                        color = if (read) Color.Gray else Color.Black
+                    Text(stringResource(R.string.main_title))
+                    RatingThumbs(
+                        likesCount,
+                        dislikesCount,
+                        onLikeClicked,
+                        onDislikeClicked
                     )
                 }
+                Text(
+                    stringResource(R.string.main_part1),
+                    modifier = Modifier.padding(bottom = 32.dp)
+                )
+                AsyncImage(
+                    model = "https://images.stopgame.ru/uploads/users/2024/526708/00393.hmp35GL.png",
+                    contentDescription = null,
+                    modifier = Modifier.padding(bottom = 32.dp)
+                        .clip(RoundedCornerShape(10.dp))
+                        .fillMaxSize()
+                )
+                Text(
+                    stringResource(R.string.main_part2),
+                    modifier = Modifier.padding(bottom = 32.dp)
+                )
+
+                ToNextArticleButton(
+                    stringResource(R.string.second_title),
+                    modifier = Modifier.padding(bottom = 32.dp),
+                    onCardClicked = onOtherArticleCardClicked,
+                    isActive = !isSecondArticleRead
+                )
             }
         }
     }
 }
 
-
+@Composable
+fun RatingThumbs(
+    likesCount: Int,
+    dislikesCount: Int,
+    onLikeClicked: () -> Unit,
+    onDislikeClicked: () -> Unit
+) {
+    Row () {
+        RateButton(
+            dislikesCount.toString(),
+            onDislikeClicked,
+            "Кнопка не нравится",
+            painterResource(id = R.drawable.img)
+        )
+        RateButton(
+            likesCount.toString(),
+            onLikeClicked,
+            "Кнопка нравится",
+            painterResource(id = R.drawable.img_1)
+        )
+    }
+}
+@Composable
+fun RateButton(
+    fingerValue: String,
+    onClicked: () -> Unit,
+    contentDescription: String,
+    painter: Painter
+) {
+    Column {
+        Icon(
+            modifier = Modifier.clickable { onClicked.invoke() },
+            painter = painter,
+            contentDescription = contentDescription
+        )
+        if (fingerValue.isNotEmpty()) {
+            Text(
+                fingerValue,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
+@Composable
+fun ToNextArticleButton(
+    cardText: String, modifier: Modifier = Modifier, onCardClicked: () -> Unit,
+    isActive: Boolean = false
+) {
+    val ifActiveColor = if (isActive) Color.LightGray else Color.Gray
+    val ifInactiveColor = if (isActive) Color.Gray else Color.DarkGray
+    Column(modifier = modifier) {
+        Text(
+            stringResource(R.string.card_article_text),
+            modifier = Modifier.padding(start = 8.dp, bottom = 8.dp)
+        )
+        OutlinedCard(
+            colors = CardDefaults.cardColors(
+                containerColor = ifInactiveColor,
+                contentColor = ifActiveColor
+            ),
+            border = BorderStroke(1.dp, ifActiveColor),
+            modifier = Modifier.clickable { onCardClicked.invoke() }
+        ) {
+            Text(
+                text = cardText,
+                modifier = Modifier.padding(16.dp),
+                textAlign = TextAlign.Center,
+            )
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun MainScreenPreview() {
-    MainActivityScreen(handler = object : MainScreenHandler {
-        override fun onToOtherScreenClicked() {
-
-        }
-
-        override fun onToShareClicked() {
-
-        }
-
-    })
+    MainActivityScreen()
 }

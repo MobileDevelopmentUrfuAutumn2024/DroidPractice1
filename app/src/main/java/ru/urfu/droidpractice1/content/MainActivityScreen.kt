adb9f03bdea009b3a42e5300d33bfba126d04f24
D-Yurtaev <mr.super.yurtaev@mail.ru>
2025-01-14 16:29:08 +0500

    YurtaevDmitry

--- app/src/main/java/ru/urfu/droidpractice1/content/MainActivityScreen.kt
+++ app/src/main/java/ru/urfu/droidpractice1/content/MainActivityScreen.kt
@@ -2,49 +2,134 @@
 
 package ru.urfu.droidpractice1.content
 
-import androidx.compose.foundation.layout.Box
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LargeTopAppBar
-import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import ru.urfu.droidpractice1.R
+import ru.urfu.droidpractice1.interfaces.MainActivityInterface
+import ru.urfu.droidpractice1.storage.Article
 import ru.urfu.droidpractice1.ui.theme.DroidPractice1Theme
 
 @Composable
-fun MainActivityScreen() {
-    DroidPractice1Theme {
-        Scaffold(modifier = Modifier.fillMaxSize(),
-            topBar = {
-                TopAppBar(
-                    title = {
-                        Text(
-                            text = stringResource(id = R.string.article_title)
+fun MainActivityScreen(
+
+    mainActivityInterface: MainActivityInterface,
+    article: Article
+    ) {
+        DroidPractice1Theme {
+            Scaffold(modifier = Modifier.fillMaxSize(),
+                bottomBar = {
+                    BottomAppBar {
+                        Icon(
+                            modifier = Modifier.padding(10.dp)
+                                .clickable { mainActivityInterface.onShareClick() },
+                            painter = painterResource(R.drawable.ic_share),
+                            contentDescription = null
                         )
                     }
-                )
-            }) { innerPadding ->
-            Box(
-                modifier = Modifier.padding(innerPadding)
-            ) {
+                },
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text(
+                                text = stringResource(id = R.string.article_title)
+                            )
+                        }
+                    )
+                }) { innerPadding ->
+                Column(
+                    modifier = Modifier
+                        .verticalScroll(rememberScrollState())
+                        .padding(innerPadding)
+                ) {
+                    Text(
+                        text = article.title
+                    )
+                    AsyncImage(
+                        placeholder = ColorPainter(Color.Gray),
+                        model = article.imageUri,
+                        modifier = Modifier.padding(innerPadding)
+                            .wrapContentSize(align = Alignment.Center)
+                            .fillMaxWidth().height(300.dp),
+                        contentDescription = null,
+                        error = ColorPainter(Color.Red),
+                    )
+                    Text(
+                        text = article.text
+                    )
+                    Row(
+                        modifier = Modifier.padding(10.dp)
+                    ) {
+                        Icon(
+                            modifier = Modifier.padding(10.dp)
+                                .clickable { mainActivityInterface.onLikeClick() },
+                            painter = painterResource(R.drawable.ic_thumbup),
+                            contentDescription = null
+                        )
+                        Text(
+                            text = article.likes.toString()
+                        )
+                        Icon(
+                            modifier = Modifier
+                                .padding(10.dp)
+                                .clickable { mainActivityInterface.onDislikeClick() },
+                            painter = painterResource(R.drawable.ic_thumbdown),
+                            contentDescription = null
+                        )
+                        Text(
+                            text = article.disLikes.toString()
+                        )
+                    }
+                    TextButton(
+                        onClick = {
+                            mainActivityInterface.onLinkClick()
+                        },
+                        modifier = Modifier
+                            .padding(vertical = 10.dp)
+                            .fillMaxWidth()
+                            .clip(RoundedCornerShape(16.dp))
+                            .background(Color(0xFFebdada))
+                    ) {
+                        Text(
+                            text = "Stan Lee",
+                            color = Color.Black,
+                        )
+                    }
 
+
+                }
             }
         }
     }
-}
 
-
-
-@Preview(showBackground = true)
 @Composable
-fun MainScreenPreview() {
-    MainActivityScreen()
+fun AsyncImage(placeholder: ColorPainter, model: String, modifier: Modifier, contentDescription: Nothing?, error: ColorPainter) {
+
 }
